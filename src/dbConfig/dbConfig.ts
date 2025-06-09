2e5539510a004d70fcac6668cfad36cd15a30f8f
Utkarsh Kumawat <utkarshkumawat77955@gmail.com>
2025-06-09 23:27:50 +0530

    fix: update MongoDB connection URI variable from MONGODB_URI to MONGO_URL

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectToDatabase() {
 	try {
-		await mongoose.connect(process.env.MONGODB_URI!);
+		await mongoose.connect(process.env.MONGO_URL!);
 		const connection = mongoose.connection;
 		connection.on("connected", () => {
 			console.log("Connected to MongoDB successfully");
